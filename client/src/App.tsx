b65164cdce323e4ff2f62144295edef90cad33b2
moshe1967 <fniddam@gmail.com>
2026-04-11 15:22:53 +0000

    deploy site github pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Router } from "wouter";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -6,7 +7,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
+function RouterRoutes() {
   return (
     <Switch>
       <Route path="/" component={Home} />
@@ -17,12 +18,14 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <Router base="/Thyroid-CoachFredericNIDDAM">
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <RouterRoutes />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
